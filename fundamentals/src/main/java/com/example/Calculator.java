93fdd2901352b5c9698c5f8b52929c91466df21a
astefan <stefan.avram@irian.ro>
2021-01-14 18:18:21 +0200

    init modules

---
+++ fundamentals/src/main/java/com/example/Calculator.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Calculator {
+
+    public static int add(int x, int y) {
+        return x + y;
+    }
+
+    public static int divide(int x, int y) {
+        return x / y;
+    }
+
+}
