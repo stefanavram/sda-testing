9dec29cbf85982a31f51e30404c8370c6e9a7d8b
astefan <stefan.avram@irian.ro>
2021-01-14 18:52:27 +0200

    exercise_3 - before after

--- fundamentals/src/test/java/com/example/TestingExercises.java
+++ fundamentals/src/test/java/com/example/TestingExercises.java
@@ -1,10 +1,35 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestingExercises {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Run before the first test method");
+    }
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("Run before each test");
+    }
+
+    @Test
+    public void test_example() {
+        assert 1 == 1;
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("Run after each test");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("Run after the last test method");
+    }
 
     @Test
     public void assert_true_or_false() {
