2cd6c453ac9f44d790a705ed4020120015f30a8e
astefan <stefan.avram@irian.ro>
2021-01-14 18:37:24 +0200

    exercise_2 - testing methods

---
+++ fundamentals/src/test/java/com/example/TestingExercises.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestingExercises {
+
+    @Test
+    public void assert_true_or_false() {
+        assertTrue(3 == 3);
+        assertFalse("Hello".equals("Gigi"));
+    }
+
+    @Test
+    public void assert_null() {
+        String s = null;
+        assertNull(s);
+
+        String notEmptyString = "Hello";
+        assertNotNull(notEmptyString);
+    }
+
+
+    @Test
+    public void assert_same() {
+        String s = new String("Hi");
+        assertSame("Hi", s);
+    }
+
+    @Test
+    public void assert_fail() {
+        fail();
+    }
+
+    @Test
+    public void test_arrays_equals() {
+        int[] array1 = {1, 2, 3};
+        int[] array2 = {1, 2, 3};
+        assertArrayEquals(array1, array2);
+    }
+}
