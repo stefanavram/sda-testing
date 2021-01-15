8db245f1d8945ca5da4031e5b1fb2b90bfe5bbac
astefan <stefan.avram@irian.ro>
2021-01-15 16:16:23 +0200

    added sub method Calculator

--- advanced/src/main/java/com/example/Calculator.java
+++ advanced/src/main/java/com/example/Calculator.java
@@ -2,10 +2,13 @@ package com.example;
 
 public class Calculator {
 
-    public int add(int x, int y) {
+    public double add(double x, double y) {
         return x + y;
     }
 
+    public double sub(double x, double y) {
+        return x - y;
+    }
     public int divide(int x, int y) {
         return x / y;
     }
